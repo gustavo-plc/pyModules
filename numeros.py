34ff597a641869d3b65d8166f0b96344cf4d22b4
gustavo-plc <gustavoparreira@hotmail.com>
2024-12-28 07:58:52 -0300

    code before modularitazion

--- numeros.py
+++ numeros.py
@@ -1,10 +1,20 @@
+#DEFINIÇÃO DAS FUNÇÕES
+
 def fatorial(n):
     i = 1
     for c in range(1, n + 1):
         i *= c
     return i
 
+def dobro(n):
+    return 2 * n
 
+def triplo(n):
+    return 3 * n
+
+
+#PROGRAMA PRINCIPAL
 num = int(input('Digite um valor: '))
 fat = fatorial(num)
 print(f'O fatorial de {num} é {fat}')
+print(f'O dobro de {num} é {dobro(num)}')
